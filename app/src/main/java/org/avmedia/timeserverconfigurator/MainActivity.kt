06075f06aa4af37dd7fb9f997cbfe3d1dfd7f5aa
Ivo Zivkov <izivkov@gmail.com>
2025-11-06 12:39:19 -0500

    Removed history logs

--- app/src/main/java/org/avmedia/timeserverconfigurator/MainActivity.kt
+++ app/src/main/java/org/avmedia/timeserverconfigurator/MainActivity.kt
@@ -2,48 +2,29 @@
 package org.avmedia.timeserverconfigurator
 
 import ConfigViewModel
-import LogsScreen
-import LogsViewModel
 import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
 import androidx.annotation.RequiresPermission
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.List
-import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material3.Icon
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import org.avmedia.timeserverconfigurator.ui.theme.TimeServerConfiguratorTheme
-
-import android.os.Build
-import androidx.annotation.RequiresApi
-import androidx.compose.ui.graphics.vector.ImageVector
 
 @RequiresApi(Build.VERSION_CODES.O)
 class MainActivity : ComponentActivity() {
 
     private val configViewModel: ConfigViewModel by viewModels {
         ViewModelFactory(this) // 'this' is a Context
-    }
-
-    private val logsViewModel: LogsViewModel by viewModels {
-        ViewModelFactory(this)
     }
 
     class ViewModelFactory(
@@ -54,10 +35,6 @@ class MainActivity : ComponentActivity() {
             if (modelClass.isAssignableFrom(ConfigViewModel::class.java)) {
                 @Suppress("UNCHECKED_CAST")
                 return ConfigViewModel(context) as T
-            }
-            if (modelClass.isAssignableFrom(LogsViewModel::class.java)) {
-                @Suppress("UNCHECKED_CAST")
-                return LogsViewModel(context) as T
             }
             throw IllegalArgumentException("Unknown ViewModel class")
         }
@@ -71,48 +48,21 @@ class MainActivity : ComponentActivity() {
         setContent {
             CheckPermissions {
 
-                logsViewModel.connect {
-                    println("Disconnected from LogsViewModel")
-                }
-
                 configViewModel.connect {
                     onConfigDisconnect()
                 }
 
                 TimeServerConfiguratorTheme {
-                    var selectedRoute by rememberSaveable { mutableStateOf(BottomNavItem.Settings.route) }
-
+                    // The Scaffold no longer has a bottomBar
                     Scaffold(
-                        modifier = Modifier.fillMaxSize(),
-                        bottomBar = {
-                            NavigationBar {
-                                BottomNavItem.values().forEach { item ->
-                                    NavigationBarItem(
-                                        icon = { Icon(item.icon, contentDescription = item.label) },
-                                        label = { Text(item.label) },
-                                        selected = selectedRoute == item.route,
-                                        onClick = { selectedRoute = item.route }
-                                    )
-                                }
-                            }
-                        }
+                        modifier = Modifier.fillMaxSize()
                     ) { innerPadding ->
-                        when (selectedRoute) {
-                            BottomNavItem.Settings.route -> {
-                                ConfigScreen(
-                                    modifier = Modifier.padding(innerPadding),
-                                    viewModel = configViewModel,
-                                    context = this
-                                )
-                            }
-
-                            BottomNavItem.Logs.route -> {
-                                LogsScreen(
-                                    modifier = Modifier.padding(innerPadding),
-                                    logsViewModel = this.logsViewModel
-                                )
-                            }
-                        }
+                        // The content is now directly set to ConfigScreen
+                        ConfigScreen(
+                            modifier = Modifier.padding(innerPadding),
+                            viewModel = configViewModel,
+                            context = this
+                        )
                     }
                 }
             }
@@ -127,7 +77,6 @@ class MainActivity : ComponentActivity() {
     @SuppressLint("MissingPermission")
     override fun onDestroy() {
         super.onDestroy()
-        logsViewModel.disconnect()
         configViewModel.disconnect()
     }
 
@@ -136,18 +85,5 @@ class MainActivity : ComponentActivity() {
         configViewModel.connect {
             onConfigDisconnect()
         }
-    }
-}
-
-private enum class BottomNavItem(
-    val route: String,
-    val label: String,
-    val icon: ImageVector
-) {
-    Settings("settings", "Settings", Icons.Filled.Settings),
-    Logs("logs", "Logs", Icons.Filled.List);
-
-    companion object {
-        fun values() = listOf(Settings, Logs)
     }
 }
